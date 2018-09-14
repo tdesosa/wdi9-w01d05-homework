4526809b008c3299537ec2be82a66f3e7a900ff1
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-14 15:30:09 -0600

    Q + A answered

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// console.log("Finish this and enjoy the weekend!");
+
+// Section 1. Variables & Datatypes
+// A. Q + A
+    // 1. var = value; (by setting the variable = to its value)
+    // 2. var = newValue; (by defining the with the new value)
+    // 3. varNew = varOld; (by setting the new variable to = the existing varaible)
+    // 4. declare: setting the variable in our scope
+    //    define: putting a variable in place 
+    //    assign: giving a variable a value
+    // 5. the act of writing out the process to execute a given request
+    //    in words rather than code with the idea being that doing so will
+    //    (does) make the process of executing the request in code easier.
+    // 6. 70% thinking 30% typing
+    
+
+
+
