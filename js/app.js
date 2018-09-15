811c82451a0cce6b2449038b5b61ed7a263f5699
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-14 18:10:13 -0600

    Whats in your Closet answered

--- js/app.js
+++ js/app.js
@@ -232,6 +232,75 @@ const findMonkey = (num) => {
 
 findMonkey(5);
 
+// H. What's in Your Closet
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+//Q1
+
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`);
+
+//Q2
+
+kristynsCloset.splice(6, 0, "raybans");
+console.log(kristynsCloset);
+
+//Q3
+
+kristynsCloset[5] = "stained knit hat";
+console.log(kristynsCloset);
+
+//Q4
+
+console.log(thomsCloset[0][0]);
+
+//Q5
+
+console.log(thomsCloset[1][0]);
+
+//Q6
+
+console.log(thomsCloset[2][2]);
+
+//Q7
+
+console.log(`Thom is wearing a ${thomsCloset[0][0]}, ${thomsCloset[1][0]}, and ${thomsCloset[2][2]}!`);
+
+//Q8
+
+thomsCloset[1][2] = "Footie Pajamas";
+console.log(thomsCloset[1][2]);
+
+
+
 
 
 
