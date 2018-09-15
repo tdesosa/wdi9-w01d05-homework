2e6437643a36d2fde3fdd113ca2f5eb5846eb1c5
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-14 20:02:26 -0600

    maxOfThree completed

--- js/app.js
+++ js/app.js
@@ -356,8 +356,17 @@ const getMultipleLengths = (arrayOfStrings) => {
 }
 console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]));
 
+// G. maxOfThree
 
-
+const maxOfThree = (num1, num2, num3) => {
+    return Math.max(num1, num2, num3);
+    if(num1 === num2|| num1 === num3) {
+        return num1;
+    } else if (num2 === num3) {
+        return num2;
+    }
+}
+console.log(maxOfThree(6, 9, 1));
 
 
 
